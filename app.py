dbcb35ec2c171d305c5bbdf4df7f882ba3b91017
Max27253773 <maxwade370@gmail.com>
2026-03-17 11:11:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -57,21 +57,21 @@ if not st.session_state["auth"]:
         pw_input = st.text_input("Mot de passe", type="password")
         submit_auth = st.form_submit_button("SE CONNECTER")
         
-           if submit_auth:
-            # Dictionnaire des accès
-            credentials = {
-                "UT": {"pw": "Azerty123*", "role": "Utilisateur"},
-                "ANIM": {"pw": "Anim2026*", "role": "Animateur"}
-            }
-            
-            if user_input in credentials and pw_input == credentials[user_input]["pw"]:
-                st.session_state["auth"] = True
-                st.session_state["role"] = credentials[user_input]["role"] # On stocke le rôle
-                st.success(f"Bienvenue, rôle : {st.session_state['role']}")
-                time.sleep(0.5)
-                st.rerun()
-            else:
-                st.error("Identifiants incorrects")
+         if submit_auth:
+                # Dictionnaire simple des accès (à adapter selon tes besoins)
+                credentials = {
+                    "UT": {"pw": "Azerty123*", "role": "Utilisateur"},
+                    "ANIM": {"pw": "Anim2026*", "role": "Animateur"}
+                }
+                
+                if user_input in credentials and pw_input == credentials[user_input]["pw"]:
+                    st.session_state["auth"] = True
+                    st.session_state["role"] = credentials[user_input]["role"] # On stocke le rôle
+                    st.success(f"Bienvenue, rôle : {st.session_state['role']}")
+                    time.sleep(0.5)
+                    st.rerun()
+                else:
+                    st.error("Identifiants incorrects")
     st.stop()
     
 # --- 4. SI CONNECTÉ : DESIGN NORMAL ---
